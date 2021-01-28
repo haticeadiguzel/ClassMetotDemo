b70030ceebfa719a495960d354e6553cc46c8d01
hatice adıgüzel <adiguzell.hatice@gmail.com>
2021-01-28 13:21:04 +0300

    Kod gönderildi.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        //İKİNCİ KULLANIM MANAGER'I
+        //Takip fonksiyonunun parametrelerine veri türleri dahil gerekenleri yazdık.
+        //Fonksiyon içini yazdık.
+        public void Takip(string Id, string Ad, string Soyad)
+        {
+            Console.WriteLine("Kullanıcı adınız: " + Id);
+            Console.WriteLine("Adınız: " + Ad);
+            Console.WriteLine("Soyadınız: " + Soyad);
+            Console.WriteLine("");
+        }
+
+        //ÜÇÜNCÜ KULLANIM MANAGER'I
+        //Takip fonksiyonunun içine alınması gereken bilgi yazılır.
+        //Fonksiyon içini yazarız.
+        public void Takip2(Musteri musteri)
+        {
+            Console.WriteLine("Kullanıcı adınız: " + musteri.Id);
+            Console.WriteLine("Adınız: " + musteri.Ad);
+            Console.WriteLine("Soyadınız: " + musteri.Soyad);
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****BİRİNCİ KULLANIM*****");
+            Console.WriteLine(" ");
+            //MusteriManager kullanılmadan sadece Musteri class'ı kullanarak yapabiliriz.
+            //Musteri class'taki verileri yukarıdaki gibi yazarız.
+            //Yazılanları bir diziye aktarırız.
+            //Diziyi foreach döngüsü ile yazdırırız.
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = "Adiguzel.dicle";
+            musteri1.Ad = "Dicle";
+            musteri1.Soyad = "Adıgüzel";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = "Adiguzel.hatice";
+            musteri2.Ad = "Hatice";
+            musteri2.Soyad = "Adıgüzel";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = "Ozdemir.umut";
+            musteri3.Ad = "Umut";
+            musteri3.Soyad = "Özdemir";
+
+            Musteri[] musteriler = new Musteri[] {musteri1, musteri2, musteri3 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Kullanıcı adınız: " + musteri.Id);
+                Console.WriteLine("Adınız: " + musteri.Ad);
+                Console.WriteLine("Soyadınız: " + musteri.Soyad);
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("*****İKİNCİ KULLANIM*****");
+            Console.WriteLine(" ");
+            //Bu kullanımda bir Musteri class'ına ek bir Manager class kullandık.
+            //MusteriManager'daki İKİNCİ KULLANIM yorumunu oku.
+            //Bu kullanımda tüm ifadeleri kendimiz parametre kısmına yazarız
+            //En üstte yazılan uzun işlemlere gerek yoktur.
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Takip("Adiguzel.dicle", "Dicle", "Adiguzel");
+            musteriManager.Takip("Adiguzel.hatice", "Hatice", "Adiguzel");
+            musteriManager.Takip("Odemir.umut", "Umut", "Özdemir");
+
+            Console.WriteLine("*****ÜÇÜNCÜ KULLANIM*****");
+            Console.WriteLine(" ");
+            //Bu kullanımda bir Musteri class'ına ek bir Manager class kullandık.
+            //MusteriManager'daki ÜÇÜNCÜ KULLANIM yorumunu oku.
+            //Bu kullanımdada ilk kullanım gibi en üstteki ifadeleri tek tek yazmak gerekir.
+
+            MusteriManager mustereriManager = new MusteriManager();
+            mustereriManager.Takip2(musteri1);
+            mustereriManager.Takip2(musteri2);
+            mustereriManager.Takip2(musteri3);
+
+
+        }
+    }
+}
